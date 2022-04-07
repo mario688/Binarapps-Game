44cd9a43a6398e6d9eed1835debeb6ec93005a16
mario688 <gliwamario@gmail.com>
2022-04-07 23:18:45 +0200

    Added function which handle click every single word and invoke check function

--- src/components/words/CategoryItem.tsx
+++ src/components/words/CategoryItem.tsx
@@ -3,15 +3,25 @@ import SingleWord from "./SingleWord";
 interface Category {
   all_words: string[];
   good_words: string[];
-  questions: string;
+  question: string;
 }
 const CategoryItem: React.FC<{ category: Category }> = (props) => {
-  const { all_words, good_words, questions } = props.category;
+  const { all_words, good_words, question } = props.category;
 
+  const CheckWordHandler = (word: string) => {
+    if (good_words.includes(word)) {
+      console.log("gitówa majonez");
+    }
+  };
   const singleWord = all_words.map((element) => (
-    <SingleWord key={element} word={element} />
+    <SingleWord key={element} word={element} onCheckWord={CheckWordHandler} />
   ));
 
-  return <>{singleWord}</>;
+  return (
+    <>
+      <h1>{question}</h1>
+      {singleWord}
+    </>
+  );
 };
 export default CategoryItem;
--- src/components/words/SingleWord.tsx
+++ src/components/words/SingleWord.tsx
@@ -1,4 +1,18 @@
-const SingleWord: React.FC<{ word: string }> = (props) => {
-  return <>{props.word}</>;
+import Style from "./SingleWord.module.scss";
+const SingleWord: React.FC<{
+  onCheckWord: (word: string) => void;
+  word: string;
+}> = (props) => {
+  const { onCheckWord } = props;
+
+  const onClickHandler = (e: any) => {
+    onCheckWord(e.target.textContent);
+  };
+
+  return (
+    <div className={Style.singleWord} onClick={onClickHandler}>
+      {props.word}
+    </div>
+  );
 };
 export default SingleWord;

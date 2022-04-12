c8805d6906f6dfa6d1efe08e6d0b36456d76280b
mario688 <gliwamario@gmail.com>
2022-04-13 00:09:44 +0200

    added modal which appear when user doesn't select one correct answer at least

--- src/components/UI/formWordsSet/Form.tsx
+++ src/components/UI/formWordsSet/Form.tsx
@@ -37,6 +37,11 @@ const Form = () => {
   const saveWordsSetHandler = () => {
     const question = questionRef!.current!.value;
     const category = categoryRef!.current!.value;
+    if (!goodWords.length) {
+      console.log("select one word at least");
+      setShowModal(true);
+      return;
+    }
     sendRequest(
       "https://sturdy-dragon-299320-default-rtdb.firebaseio.com/wordslist.json",
       "POST",
@@ -65,6 +70,15 @@ const Form = () => {
           message={"This word is already in set"}
         />
       )}
+      {!goodWords.length && showModal && (
+        <Modal
+          onClickHandler={() => {
+            setShowModal((prev) => !prev);
+          }}
+          title="INFO"
+          message={"Question has to at least one correct word"}
+        />
+      )}
       <div className={Style.inputsContainer}>
         <Input
           label="Question"
@@ -74,7 +88,7 @@ const Form = () => {
             id: "question",
             required: true,
             placeholder: "question...",
-            minLength: 5,
+            minLength: 2,
           }}
         />
         <Input
@@ -85,7 +99,7 @@ const Form = () => {
             id: "category",
             required: true,
             placeholder: "category...",
-            minLength: 5,
+            minLength: 2,
           }}
         />
 
@@ -97,7 +111,7 @@ const Form = () => {
             id: "word",
             required: true,
             placeholder: "new word...",
-            minLength: 5,
+            minLength: 3,
           }}
         />
         <div onClick={addWordHandler} className={Style.addButton}></div>
